2d8771666b74135c3fbcb6e6697024e69e70b9c7
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-18 10:01:41 -0300

    findAll paged

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -1,5 +1,29 @@
 package com.devsuperior.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.services.EventService;
+
+@RestController
+@RequestMapping(value = "/events")
 public class EventController {
 
+	@Autowired
+	private EventService service;
+	
+	//@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+	@GetMapping
+	public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+		PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+		Page<EventDTO> list = service.findAll(pageRequest);		
+		return ResponseEntity.ok().body(list);
+	}
 }
