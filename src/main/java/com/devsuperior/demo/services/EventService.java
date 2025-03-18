9559611994d47bb5c93dbd7c007b447c0a79961a
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-18 10:55:53 -0300

    Event Tests completed

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.entities.Event;
 import com.devsuperior.demo.repositories.EventRepository;
 
@@ -20,5 +21,23 @@ public class EventService {
 	public Page<EventDTO> findAll(Pageable pageable) {
 		Page<Event> page = repository.findAll(pageable);
 		return page.map(x -> new EventDTO(x));
+	}
+
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event entity = new Event();
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setUrl(dto.getUrl());
+		
+		//entity.setCity(new City(dto.getCityId(), null));
+		
+		entity.setCity(new City() {{
+			setId(dto.getCityId());
+		}});
+		
+		entity = repository.save(entity);
+		
+		return new EventDTO(entity);
 	}
 }
